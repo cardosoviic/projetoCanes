67c74218ad04ed875748274a39ba9d97a2040fe7
cardosoviic <victoriacardoso545@gmail.com>
2023-12-06 11:56:25 -0300

    Adicionado o Script e o Diagrama do Banco De Dados

---
+++ bancoDeDados/script.sql
@@ -0,0 +1,72 @@
+create database dbCanes2;
+
+use dbCanes2;
+
+create table funcionarios (
+idFunc int primary key auto_increment,
+nomeFunc varchar(60) not null,
+login varchar(20) not null unique,
+email varchar(50) not null unique,
+senha varchar(255) not null,
+departamento enum("Administração", "Lideres", "Vendedores Locais",
+ "Vendedores Externos", "Logística", "Diretoria") 
+);
+
+create table fornecedores (
+idForn int primary key auto_increment,
+nomeForn varchar(50) not null,
+cnpjForn varchar(20) not null unique,
+telForn varchar(20) not null,
+emailForn varchar (50) not null unique,
+cep varchar (9),
+enderecoForn varchar(100),
+numeroEndereco varchar(10),
+bairro varchar(40),
+cidade varchar(40),
+estado char(2)
+);
+
+create table produtos (
+idProd int primary key auto_increment,
+nomeProd varchar(50) not null, 
+descricaoProd text, 
+precoProd decimal (10,2) not null, 
+estoqueProd int not null,
+categoriaProd enum ("Limpeza", "Higiene"), 
+idForn int not null,
+foreign key (idForn) references fornecedores (idForn)
+);
+
+create table clientes (
+idCliente int primary key auto_increment,
+nome varchar(100),
+cpf varchar(14) not null unique,
+cnpj varchar(20) unique,
+tel varchar(20),
+email varchar(50) unique,
+cep varchar (9),
+endereco varchar(100),
+numEndereco varchar(10),
+bairro varchar(40),
+cidade varchar(40),
+estado char(2)
+);
+
+create table pedidos (
+idPed int primary key auto_increment,
+dataPed timestamp default current_timestamp,
+statusPed enum("Pendente", "Finalizado", "Cancelado") not null,
+idFunc int not null,
+foreign key (idFunc) references funcionarios (idFunc),
+idCliente int not null,
+foreign key (idCliente) references clientes (idCliente)
+);
+
+create table itensPedidos (
+iditensPed int primary key auto_increment, 
+idPed int not null,
+idProd int not null,
+foreign key (idPed) references pedidos(idPed),
+foreign key (idProd) references produtos(idProd),
+quantidade int not null
+);
